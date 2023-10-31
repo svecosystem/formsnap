50662190f52e9f18b4f043fe3767539ea78f4d1b
Hunter Johnston <johnstonhuntera@gmail.com>
2023-10-31 10:43:41 -0400

    add testing config

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vitest/config";
 import { fileURLToPath, URL } from "url";
@@ -5,7 +6,15 @@ import { fileURLToPath, URL } from "url";
 export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
-		include: ["src/**/*.{test,spec}.{js,ts}"]
+		include: ["src/**/*.{test,spec}.{js,ts}"],
+		globals: true,
+		environment: "jsdom",
+		includeSource: ["src/**/*.{js,ts,svelte}"],
+		setupFiles: ["./other/setupTest.ts"],
+		coverage: {
+			exclude: ["setupTest.ts"]
+		},
+		alias: [{ find: /^svelte$/, replacement: "svelte/internal" }]
 	},
 	server: {
 		fs: {
