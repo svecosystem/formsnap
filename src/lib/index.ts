d58d9bdce5898304647495761992dfc9dddb9779
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2024-01-03 13:45:39 -0500

    added type

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,5 @@
 // Reexport your entry components here
 export * as Form from "./components/index.js";
-export * from "./types.js";
 export * from "./helpers/index.js";
 export * from "./internal/super-form-patch.js";
+export type * from "./types.js";
