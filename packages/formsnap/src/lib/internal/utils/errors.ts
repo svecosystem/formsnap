4cc8fd1c5cfd46970c60ea9810270c4c9d4e5a58
Hunter Johnston <johnstonhuntera@gmail.com>
2024-03-27 12:08:50 -0400

    cleanup console logs

--- packages/formsnap/src/lib/internal/utils/errors.ts
+++ packages/formsnap/src/lib/internal/utils/errors.ts
@@ -6,18 +6,10 @@ import type { ValidationErrors } from 'sveltekit-superforms';
 export function extractErrorArray<T extends Record<string, unknown>>(
 	errors: ValidationErrors<T> | undefined
 ): string[] {
-	if (Array.isArray(errors)) {
-		console.log(errors);
-		return errors;
-	}
-
+	if (Array.isArray(errors)) return errors;
 	if (typeof errors === 'object' && '_errors' in errors) {
-		if (errors._errors !== undefined) {
-			console.log(errors._errors);
-			return errors._errors;
-		}
+		if (errors._errors !== undefined) return errors._errors;
 	}
 
-	console.log([]);
 	return [];
 }
