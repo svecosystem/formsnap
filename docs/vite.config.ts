4bb5f0bbcef010f144439e3a6bfc04d343b8811c
Hunter Johnston <johnstonhuntera@gmail.com>
2024-12-12 22:02:55 -0500

    shutup check

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -6,6 +6,7 @@ import { resolve } from "node:path";
 const __dirname = new URL(".", import.meta.url).pathname;
 
 export default defineConfig({
+	// @ts-expect-error - shh
 	plugins: [sveltekit(), tailwindcss()],
 	optimizeDeps: {
 		exclude: ["formsnap"],
